77a2196e3a00730d103926dd4bd74bf12e7c416c
malcolm maima <malcolmmaima@gmail.com>
2023-06-20 14:52:53 +0300

    fetch questions viewmodel

--- app/src/main/java/com/malcolmmaima/teamwaypersonality/ui/personalitytest/MainActivity.kt
+++ app/src/main/java/com/malcolmmaima/teamwaypersonality/ui/personalitytest/MainActivity.kt
@@ -1,17 +1,35 @@
 package com.malcolmmaima.teamwaypersonality.ui.personalitytest
 
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import com.malcolmmaima.teamwaypersonality.databinding.ActivityMainBinding
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    private val viewModel = MainViewModel()
+    private val viewModel: MainViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        fetchPersonalityQuestions()
+    }
 
+    private fun fetchPersonalityQuestions() {
+        viewModel.fetchPersonalityQuestions()
+
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.personalityQuestions.collectLatest { personalityQuestions ->
+                    // Update the UI.
+                }
+            }
+        }
     }
 }
--- app/src/main/java/com/malcolmmaima/teamwaypersonality/ui/personalitytest/MainViewModel.kt
+++ app/src/main/java/com/malcolmmaima/teamwaypersonality/ui/personalitytest/MainViewModel.kt
@@ -1,9 +1,45 @@
 package com.malcolmmaima.teamwaypersonality.ui.personalitytest
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.malcolmmaima.teamwaypersonality.data.models.PersonalityQuestionsResponse
+import com.malcolmmaima.teamwaypersonality.data.repository.AppRepository
+import com.malcolmmaima.teamwaypersonality.network.APIResource
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class MainViewModel @Inject constructor(
-) : ViewModel() {}
+    val appRepository: AppRepository
+) : ViewModel() {
+    private val _personalityQuestions = MutableSharedFlow<PersonalityQuestionsResponse>()
+    val personalityQuestions = _personalityQuestions.asSharedFlow()
+
+    private val _isLoading = MutableSharedFlow<Boolean>()
+    val isLoading = _isLoading.asSharedFlow()
+
+    private val _errorMessage = MutableSharedFlow<String>()
+    val errorMessage = _errorMessage.asSharedFlow()
+
+    fun fetchPersonalityQuestions() {
+            viewModelScope.launch {
+                val personalityQuestions = appRepository.getPersonalityQuestions()
+                when (personalityQuestions) {
+                    is APIResource.Success -> {
+                        _personalityQuestions.emit(personalityQuestions.value)
+                        Log.d("MainViewModel", "fetchPersonalityQuestions: ${personalityQuestions.value}")
+                    }
+                    is APIResource.Loading -> {
+                        _isLoading.emit(true)
+                    }
+                    is APIResource.Error -> {
+                        _errorMessage.emit(personalityQuestions.errorBody.toString())
+                    }
+                }
+            }
+        }
+}
