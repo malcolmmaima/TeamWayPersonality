5281a29b187b9b8e144244af2c583dfce028a04e
malcolm maima <malcolmmaima@gmail.com>
2023-06-21 13:49:09 +0300

    add MainViewModelTest

--- app/src/test/java/com/malcolmmaima/teamwaypersonality/ui/personalitytest/viewmodel/MainViewModelTest.kt
+++ app/src/test/java/com/malcolmmaima/teamwaypersonality/ui/personalitytest/viewmodel/MainViewModelTest.kt
@@ -1,13 +1,18 @@
 package com.malcolmmaima.teamwaypersonality.ui.personalitytest.viewmodel
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.malcolmmaima.teamwaypersonality.data.models.Option
 import com.malcolmmaima.teamwaypersonality.data.models.PersonalityQuestionsResponse
+import com.malcolmmaima.teamwaypersonality.data.models.Question
+import com.malcolmmaima.teamwaypersonality.data.models.Traits
 import com.malcolmmaima.teamwaypersonality.data.repository.AppRepository
 import com.malcolmmaima.teamwaypersonality.network.APIResource
+import com.malcolmmaima.teamwaypersonality.utils.CoroutineCustomRule
 import io.mockk.coEvery
 import io.mockk.mockk
 import io.mockk.unmockkAll
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.test.runTest
 import org.junit.After
 import org.junit.Before
@@ -19,6 +24,9 @@ class MainViewModelTest {
     // Executes each task synchronously using Architecture Components.
     @get:Rule
     val instantExecutorRule = InstantTaskExecutorRule()
+
+    @get:Rule
+    val customCoroutineRule = CoroutineCustomRule()
 
     private lateinit var viewModel: MainViewModel
     private val appRepository = mockk<AppRepository>(relaxed = true)
@@ -33,17 +41,41 @@ class MainViewModelTest {
         unmockkAll()
     }
 
-//    @Test
-//    fun `fetchPersonalityQuestions emits success result`() = runTest {
-//        // Mock APIResource.Success response
-//        coEvery { appRepository.getPersonalityQuestions() } returns APIResource.Success(
-//            PersonalityQuestionsResponse(
-//                listOf()
-//            )
-//        )
-//
-//        viewModel.fetchPersonalityQuestions()
-//
-//        // Assert that the emitted value is the expected one
-//    }
+    @Test
+    fun `fetchPersonalityQuestions emits success result`() = runTest {
+        // Mock APIResource.Success response
+        val expectedResponse = APIResource.Success(
+            PersonalityQuestionsResponse(
+                listOf(
+                    Question(
+                        id = "1",
+                        question = "Personality Question 1",
+                        options = listOf(
+                            Option(
+                                id = "A",
+                                text = "Option A",
+                                traits = Traits(
+                                    openness = 0.0,
+                                    conscientiousness = 0.0,
+                                    extraversion = 0.0,
+                                    agreeableness = 0.0,
+                                    neuroticism = 0.0
+                                )
+                            )
+                        )
+                    )
+                )
+            )
+        )
+
+        coEvery { appRepository.getPersonalityQuestions() } returns expectedResponse
+
+        viewModel.fetchPersonalityQuestions()
+
+        // Collect the emitted value from the shared flow
+        val actualResponse = viewModel.personalityQuestions
+
+        // Assert that the actual response is equal to the expected response
+        assert(APIResource.Success(actualResponse.first()).value == expectedResponse.value)
+    }
 }
---
+++ app/src/test/java/com/malcolmmaima/teamwaypersonality/utils/CoroutineCustomRule.kt
@@ -0,0 +1,30 @@
+package com.malcolmmaima.teamwaypersonality.utils
+
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.StandardTestDispatcher
+import kotlinx.coroutines.test.TestDispatcher
+import kotlinx.coroutines.test.UnconfinedTestDispatcher
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.setMain
+import org.junit.rules.TestWatcher
+import org.junit.runner.Description
+
+/**
+ * Sets the main dispatcher to a test dispatcher, and then resets it back to the default when the
+ * test is finished
+ */
+@OptIn(ExperimentalCoroutinesApi::class)
+class CoroutineCustomRule constructor(
+    private val dispatcher: TestDispatcher = StandardTestDispatcher()
+) : TestWatcher() {
+    override fun starting(description: Description) {
+        super.starting(description)
+        Dispatchers.setMain(dispatcher)
+    }
+
+    override fun finished(description: Description) {
+        super.finished(description)
+        Dispatchers.resetMain()
+    }
+}
